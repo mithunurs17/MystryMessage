6ea980e03e3a1247439aa1244732a172865538ae
mithunurs17 <mithunursmithun@gamil.com>
2026-02-17 15:41:36 +0530

    Add Zod validation schemas and update dependencies

---
+++ src/app/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod'
+
+export const usernameValidation = z
+    .string()
+    .min(2, "Username must be atleast 2 characters")
+    .max(20, "Username must be less than 20 characters")
+    .regex(/^[a-xA-Z0-9_]+$/, "Username can only contain letters, numbers and underscores")
+
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({message: "Invalid email"}),
+    password: z.string().min(6, "Password must be atleast 6 characters")
+})
